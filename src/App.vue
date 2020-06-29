f5a6b8b22fd10668811d5424b41289a02095adfb
Miguel Alex Cantu <miguel.can2@gmail.com>
2020-06-28 20:45:17 -0500

    Added background and styling
    NamePlate has been added to display my name with styling.
    A background image has also been added.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NamePlate />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NamePlate from './components/NamePlate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NamePlate
   }
 }
 </script>
 
 <style>
 #app {
+  background-image: url("./assets/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: fixed;
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NamePlate.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>Miguel Alex Cantu</h1>
+  </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  top: 50%;
+  position: absolute;
+  color: white;
+  text-align: center;
+  left: 50%;
+  margin-left: -135px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
